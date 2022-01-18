d894e298e3ca489ddaa3c2589b7e7f7681010120
rak <dv.rakry@gmail.com>
2022-01-18 09:20:26 +0900

    2022.01.18 commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <div>
-    <h4>{{products[0]}}</h4>
+
+  <div class="menu">
+    <a v-for="작명 in 메뉴들" :key="작명">{{작명}}</a>
+  </div>
+
+  <div v-for="(반복,i) in 3" :key="(반복,i)">
+    <h4>{{products[i]}}</h4>
     <p>50 만원</p>
-  </div>
-  <div>
-    <h4>{{products[1]}}</h4>
-    <p>60 만원</p>
-  </div>
-  <div>
-    <h4>{{products[2]}}</h4>
-    <p>65 만원</p>
   </div>
 
 </template>
 
 <script>
 
+
 export default {
   name: 'App',
   data(){
     return {
+      메뉴들 : ['Home', 'Shop', 'About'],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
     }
   },
@@ -37,6 +35,16 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding : 10px;
 }
 </style>
