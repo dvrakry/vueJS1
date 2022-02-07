5d59044586225e1b279c0d58a83e8381b6b1fe14
rak <dv.rakry@gmail.com>
2022-02-07 09:13:26 +0900

    2022-02-07

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
 
   <Discount/>
 
-  <Card @openModal="모달창열렸니=true; 누른거 = $event" :원룸="원룸들[i]" v-for="(a,i) in 원룸들" :key="a"/>
+  <Card @openModal="모달창열렸니=true; 누른거=$event" :원룸="원룸들[i]" v-for="(a,i) in 원룸들" :key="a"/>
   <!-- <Card  :원룸="원룸들[1]"/>
   <Card  :원룸="원룸들[2]"/>
   <Card  :원룸="원룸들[3]"/>
